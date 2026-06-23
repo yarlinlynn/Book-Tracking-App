2ce70239089d26750ec6d47df4fdff8326e48d19
Yarlin <yarlinlynn@gmail.com>
2026-06-23 15:47:21 +0200

    Re-structured my project by adding admin folder with relevant html, css, js folders and moving elements/styles for each file accordingly

---
+++ admin/js/app.js
@@ -0,0 +1,9 @@
+
+// import { initializeLogin } from "./loginForm.js";
+import { navigation } from "./navigation.js";
+
+// initializeLogin();
+navigation();
+
+
+
--- assets/js/app.js
+++
@@ -1,9 +0,0 @@
-
-import { initializeLogin } from "./loginForm.js";
-import { navigation } from "./navigation.js";
-
-initializeLogin();
-navigation();
-
-
-
